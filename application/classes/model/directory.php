0a8a3dfbbb972611ea9df4ff65eb7bb20bfa145f
Vit Brunner <vit.brunner@gmail.com>
2011-12-30 18:59:53 +0100

    refactor getting files/dirs from directory

--- application/classes/model/directory.php
+++ application/classes/model/directory.php
@@ -23,24 +23,37 @@ class Model_Directory
 	}
 
 	/**
+	 * Get items from directory that pass check function
+	 *
+	 * @param   function  skip item?
+	 * @return  array     items from directory
+	 */
+	protected function get_items($check_function)
+	{
+		$items = array();
+		$this->dir->rewind();
+
+		foreach ($this->dir as $item) {
+			if ($check_function($item))
+				continue;
+
+			$items[] = (string) $item;
+		}
+
+		sort($items);
+		return $items;
+	}
+
+	/**
 	 * Get subdirectories of current directory
 	 *
 	 * @return  array  subdirectories
 	 */
 	public function get_dirs()
 	{
-		$dirs = array();
-		$this->dir->rewind();
-
-		foreach ($this->dir as $dir) {
-			if (! $dir->isDir() || $dir->isDot())
-				continue;
-
-			$dirs[] = (string) $dir;
-		}
-
-		sort($dirs);
-		return $dirs;
+		return $this->get_items(function($item) {
+			return (! $item->isDir() || $item->isDot());
+		});
 	}
 
 	/**
@@ -50,20 +63,11 @@ class Model_Directory
 	 */
 	public function get_files()
 	{
-		$files = array();
-		$this->dir->rewind();
+		return $this->get_items(function($item) {
+			$match = preg_quote(Arr::get(Kohana::$config->load('settings')
+				->get('thumbnail'), 'prefix'), '/');
 
-		$prefix = Kohana::$config->load('settings.thumbnail.prefix');
-		$regex = preg_quote($prefix, '/');
-
-		foreach ($this->dir as $file) {
-			if (! $file->isFile() || preg_match("/$regex/", $file))
-				continue;
-
-			$files[] = (string) $file;
-		}
-
-		sort($files);
-		return $files;
+			return (! $item->isFile() || preg_match("/^$match/", $item));
+		});
 	}
 }
