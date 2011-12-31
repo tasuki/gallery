95f8bb18e583000e0cb32d419aa50a6bcaaaa173
Vit Brunner <vit.brunner@gmail.com>
2011-12-31 19:17:06 +0100

    add forgotten footer view, add .gitignore's

---
+++ modules/gallery_v1/views/gallery/footer.php
@@ -0,0 +1,16 @@
+<div id="bottom">
+
+	<div id="byncsa">
+		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png" /></a>&nbsp;&nbsp;&nbsp;These photos are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-Noncommercial-Share Alike 3.0 License</a>.
+	</div>
+
+	<div id="calibration">
+		<?php
+			for ($i = 26; $i > 0; $i--) {
+				echo "<div class='cal$i'></div>";
+			}
+		?>
+		<div class="clear"></div>
+	</div>
+
+</div>
