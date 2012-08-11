8361ca8fe5ff20d7c0c31257ce24ddf03f7693bf
Vit Brunner <vit.brunner@gmail.com>
2012-08-11 14:28:16 +0200

    clear footer div

--- modules/gallery_v1/views/gallery/footer.php
+++ modules/gallery_v1/views/gallery/footer.php
@@ -1,5 +1,4 @@
 <div id="bottom">
-
 	<p id="byncsa">
 		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png" /></a>&nbsp;&nbsp;&nbsp;These photos are licensed under the <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-Noncommercial-Share Alike 3.0 License</a>.
 	</p>
@@ -11,4 +10,5 @@
 		<div class="clear"></div>
 	</div>
 
+	<div class="clear"></div>
 </div>
