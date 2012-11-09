f61ae26e75d067403155ef88e328e56bfe70d690
Vit Brunner <vit.brunner@gmail.com>
2012-11-09 22:39:32 +0000

    add Model_Filemapper

---
+++ application/classes/model/filemapper.php
@@ -0,0 +1,110 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Filemapper model
+ *
+ * @package  Gallery
+ * @category Model
+ * @author   Vit 'tasuki' Brunner
+ * @license  GPL
+ */
+class Model_Filemapper
+{
+	/**
+	 * @var  array  List of directories
+	 */
+	protected $dirs = array();
+
+	/**
+	 * @var  array  List of files
+	 */
+	protected $files = array();
+
+	/**
+	 * Create filemapper model
+	 *
+	 * @param  string  path to directory
+	 */
+	public function __construct($source, $destination, $mapping)
+	{
+		$this->recursively_process_dir($source, $destination, $mapping);
+	}
+
+	/**
+	 * Recursively get missing directories and files
+	 *
+	 * @param  string  source directory
+	 * @param  string  destination directory
+	 */
+	protected function recursively_process_dir($source, $destination, $mapping)
+	{
+		$src = new Model_Directory($source);
+
+		try {
+			// Try to get destination directory, with all files
+			$dst = new Model_Directory($destination);
+			$dst_dirs_missing = $dst->missing($src, Model_Directory::DIRS);
+			$dst_files = $dst->get_files();
+		} catch (UnexpectedValueException $e) {
+			// Destination dir doesn't exist, hence all src dirs
+			// are missing from dst and there are no files there.
+			$dst_dirs_missing = $src->get_dirs();
+			$dst_files = array();
+		}
+
+		foreach ($dst_dirs_missing as $dir) {
+			$this->dirs[] = array(
+				'src' => "$source/$dir",
+				'dst' => "$destination/$dir",
+			);
+		}
+
+		// Get missing files
+		foreach ($src->get_files() as $src_file) {
+			// Source file
+			$file = array('src' => "$source/$src_file");
+			foreach ($mapping as $type => $prefix) {
+				$dst_file = $prefix . $src_file;
+
+				// Is destination file missing?
+				if (! in_array($dst_file, $dst_files)) {
+					$file[$type] = "$destination/$dst_file";
+				}
+			}
+
+			// If any destination is missing, add file
+			if (count($file) > 1) {
+				$this->files[] = $file;
+			}
+		}
+
+		// Process subdirectories
+		foreach ($src->get_dirs() as $dir) {
+			$this->recursively_process_dir(
+				"$source/$dir",
+				"$destination/$dir",
+				$mapping
+			);
+		}
+	}
+
+	/**
+	 * Get next directory
+	 *
+	 * @return  array  directory
+	 */
+	public function next_dir()
+	{
+		return array_shift($this->dirs);
+	}
+
+	/**
+	 * Get next file
+	 *
+	 * @return  array  directory
+	 */
+	public function next_file()
+	{
+		return array_shift($this->files);
+	}
+}
---
+++ application/tests/classes/model/filemapper.php
@@ -0,0 +1,64 @@
+<?php defined('SYSPATH') or die('No direct access allowed!');
+
+/**
+ * Filemapper test
+ *
+ * @package  Gallery
+ * @category Test
+ * @author   Vit 'tasuki' Brunner
+ * @license  GPL
+ */
+class Model_FilemapperTest extends Kohana_UnitTest_TestCase
+{
+	/**
+	 * Test Filemapper with real filesystem (tm)
+	 */
+	public function test_mapping()
+	{
+		$mapper = new Model_Filemapper(
+			'test_data/plants',
+			'test_data/processed_plants',
+			array(
+				'orig'   => '',
+				'prefix' => '__',
+			)
+		);
+
+		$dirs = array(
+			array(
+				'src' => 'test_data/plants/flowers',
+				'dst' => 'test_data/processed_plants/flowers',
+			),
+			array(
+				'src' => 'test_data/plants/trees',
+				'dst' => 'test_data/processed_plants/trees',
+			),
+			array(
+				'src' => 'test_data/plants/trees/jehličnaté',
+				'dst' => 'test_data/processed_plants/trees/jehličnaté',
+			),
+		);
+		while ($dir = array_shift($dirs)) {
+			$this->assertEquals($mapper->next_dir(), $dir);
+		}
+
+		$files = array(
+			array(
+				'src'    => 'test_data/plants/daisies.jpg',
+				'prefix' => 'test_data/processed_plants/__daisies.jpg',
+			),
+			array(
+				'src'    => 'test_data/plants/lilies.jpg',
+				'orig'   => 'test_data/processed_plants/lilies.jpg',
+			),
+			array(
+				'src'    => 'test_data/plants/algae/algae.jpg',
+				'prefix' => 'test_data/processed_plants/algae/__algae.jpg',
+				'orig'   => 'test_data/processed_plants/algae/algae.jpg',
+			),
+		);
+		while ($file = array_shift($files)) {
+			$this->assertEquals($mapper->next_file(), $file);
+		}
+	}
+}
